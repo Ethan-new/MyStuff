3287b5c81f1f8e85250071d51819b0f3bfbecf39
Ethan Um <ethanu.email@gmail.com>
2024-03-31 15:25:21 -0400

    Added New Pics To Dashboard with Getting started button

--- my-app/src/components/homePage.js
+++ my-app/src/components/homePage.js
@@ -2,15 +2,25 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { auth } from "../config/firebase";
 
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import ResponsiveAppBar from "./navbar";
 import Stack from "@mui/material/Stack";
 
+import { Link, useNavigate } from "react-router-dom";
+
 import dashboardPic from "../assets/dashboard.png";
+import tagPic from "../assets/tags.png";
+import searchPic from "../assets/search.png";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  function goToSignUpPage() {
+    navigate("/newaccount", { replace: true });
+  }
+
   return (
     <div className="homepage_gradient_background">
       <ResponsiveAppBar></ResponsiveAppBar>
@@ -44,6 +54,21 @@ const HomePage = () => {
           </Typography>
 
           <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            marginBottom="20px"
+          >
+            <Button
+              onClick={goToSignUpPage}
+              color="success"
+              variant="contained"
+            >
+              Getting Started
+            </Button>
+          </Box>
+
+          <Box
             component="img"
             sx={{
               height: 400,
@@ -53,9 +78,10 @@ const HomePage = () => {
               //maxHeight: { xs: 233, md: 167 },
               //maxWidth: { xs: 350, md: 250 },
             }}
-            alt="The house from the offer."
+            alt="Pic of Dashboard"
             src={dashboardPic}
           />
+
           <Typography
             variant="subtitle1"
             fontSize={42}
@@ -66,29 +92,7 @@ const HomePage = () => {
           >
             Powerful Management System
           </Typography>
-          <Typography
-            variant="subtitle1"
-            fontSize={32}
-            fontWeight={600}
-            align="left"
-            color={"white"}
-          >
-            for your inventory
-          </Typography>
 
-          <Stack
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
-              placeholder
-            </Box>
-            <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
-              placeholder
-            </Box>
-          </Stack>
           <Typography
             variant="subtitle1"
             fontSize={42}
@@ -104,11 +108,53 @@ const HomePage = () => {
             justifyContent="center"
             alignItems="center"
           >
-            <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
-              placeholder
+            <Box>
+              <Box
+                component="img"
+                sx={{
+                  height: 300,
+                  width: 200,
+                  borderRadius: "16px",
+                  boxShadow: 3,
+                  //maxHeight: { xs: 233, md: 167 },
+                  //maxWidth: { xs: 350, md: 250 },
+                }}
+                alt="Pic of Dashboard"
+                src={tagPic}
+              />
+              <Typography
+                variant="subtitle1"
+                fontSize={26}
+                fontWeight={600}
+                align="left"
+                color={"white"}
+              >
+                Item Tagging
+              </Typography>
             </Box>
-            <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
-              placeholder
+            <Box>
+              <Box
+                component="img"
+                sx={{
+                  height: 300,
+                  width: 500,
+                  borderRadius: "16px",
+                  boxShadow: 3,
+                  //maxHeight: { xs: 233, md: 167 },
+                  //maxWidth: { xs: 350, md: 250 },
+                }}
+                alt="Pic of Dashboard"
+                src={searchPic}
+              />
+              <Typography
+                variant="subtitle1"
+                fontSize={26}
+                fontWeight={600}
+                align="left"
+                color={"white"}
+              >
+                Advanced Search
+              </Typography>
             </Box>
           </Stack>
         </Grid>
